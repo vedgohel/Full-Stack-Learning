44bbbddde4070164f0c960e95eba7f8d8d30e0d4
vedgohel <vedgohel85@gmail.com>
2026-04-16 16:06:06 +0530

    Sigma Javascript practice 9

---
+++ Assigment/Javascript/Part-9/script.js
@@ -0,0 +1,129 @@
+// // JavaScript (Part 9) – Practice Questions
+
+// // Q1.
+// // Create a new input and button element on the page using JavaScript only.
+// // Set the text of the button to "Click me".
+
+
+// // // Ans 1: Create input and button elements
+// // let button = document.createElement("button");
+// // let input = document.createElement("input");
+
+// // button.innerText = "Click me";
+
+// // document.body.append(input);
+// // document.body.append(button);
+
+
+
+
+
+// // Q2.
+// // Add the following attributes to the elements:
+
+// // Change the placeholder value of the input to "username"
+// // Change the id of the button to "btn"
+
+
+// // Ans 2: Add attributes
+// button.setAttribute("id", "btn");
+// input.setAttribute("placeholder", "username");
+
+
+
+
+
+
+
+
+
+
+
+// // Q3.
+// // Access the button using querySelector and its id.
+// // Then:
+
+// // Change the button’s background color to blue
+// // Change the text color to white
+
+
+
+// // // Ans 3: Style the button using querySelector
+// // let btn = document.querySelector("#btn");
+
+// // btn.style.backgroundColor = "blue";
+// // btn.style.color = "white";
+
+
+
+
+
+// // Q4.
+// // Create an h1 element on the page and set its text to "DOM Practice".
+
+// // Underline the text
+// // Change its color to purple
+
+
+
+// // // Ans 4: Create h1 element
+// // let h1 = document.createElement("h1");
+// // h1.innerHTML = "<u>DOM Practice</u>";
+// // h1.style.color = "purple";
+
+// // document.body.append(h1);
+
+
+// // Q5.
+// // Create a p tag on the page and set its text to:
+// // "Apna College Delta Practice"
+
+// // Make the word "Delta" bold
+
+
+
+// // // Ans 5: Create paragraph with bold text
+// // let p = document.createElement("p");
+// // p.innerHTML = "Apna College <b>Delta</b> Practice";
+
+// // document.body.append(p);
+
+
+
+
+
+// // Ans 1: Create input and button elements
+// let button = document.createElement("button");
+// let input = document.createElement("input");
+
+// button.innerText = "Click me";
+
+// document.body.append(input);
+// document.body.append(button);
+
+
+// // Ans 2: Add attributes
+// button.setAttribute("id", "btn");
+// input.setAttribute("placeholder", "username");
+
+
+// // Ans 3: Style the button using querySelector
+// let btn = document.querySelector("#btn");
+
+// btn.style.backgroundColor = "blue";
+// btn.style.color = "white";
+
+
+// // Ans 4: Create h1 element
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "<u>DOM Practice</u>";
+// h1.style.color = "purple";
+
+// document.body.append(h1);
+
+
+// // Ans 5: Create paragraph with bold text
+// let p = document.createElement("p");
+// p.innerHTML = "Apna College <b>Delta</b> Practice";
+
+// document.body.append(p);
