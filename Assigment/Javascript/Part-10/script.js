21389c65954de25edb048ee4b9b827f91be87811
vedgohel <vedgohel85@gmail.com>
2026-04-18 12:59:49 +0530

    Sigma Javascript practice 10

---
+++ Assigment/Javascript/Part-10/script.js
@@ -0,0 +1,33 @@
+// Q1.
+
+// Try implementing the following events using addEventListener() in JavaScript:
+
+// mouseout
+// keypress
+// scroll
+// load
+// (Take reference from MDN if needed)
+
+
+
+
+
+
+
+// Q2.
+
+// Create a button using JavaScript. Add an event listener so that when the button is clicked, its color changes to green.
+
+// Q3.
+
+// Create:
+
+// An input field with placeholder "Enter your name"
+// An H2 heading
+
+// Requirements:
+
+// The input should accept only letters (a–z, A–Z) and spaces
+// All other characters should be ignored
+// Whatever the user types should be displayed live inside the H2 heading
+// Invalid characters must NOT appear in the heading
