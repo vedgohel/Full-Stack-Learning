9eac8b0d18118e6888e6ad85bd9d732e020da2bd
vedgohel <vedgohel85@gmail.com>
2026-04-20 21:00:28 +0530

    Terminal

---
+++ terminal/script.js
@@ -0,0 +1,61 @@
+// first video
+
+// the terminal 
+// => A text input and output environment
+
+
+// Different Terms : 
+
+// 1 . command Line : any interface that is  used by entering textual command
+// 2 . Terminal : this is a type of command line.
+// 3. console : A command line interface used to work with your computer
+
+
+// 4. shell : A program running on terminal 
+
+
+// 5. bash : A popular shell on mac Os / linux
+// 6.z-shell :another shell
+
+
+
+// Basic 
+
+// ls = list files (show my files)
+// pwd = print working directory (where am i ?)
+// clear = clear screen
+
+
+
+// Navigation 
+// => inside & outside directories
+// => cd = change directory
+// => cd .. = back button / return 
+
+
+// Paths => Absolute & Relative
+
+// / => root directory
+// ~ => home directory
+
+
+// making directories
+
+// mkdir = make directory
+
+
+
+// Flags => Flags are characters that we pass with commands to modify their behaviour
+
+// manual command
+// man ls - give info about ls command
+// man mkdir - give info about mkdir command
+
+
+// Deleting the files  & folders
+
+// rm => remove files
+// rmdir => removes empty folders
+// rm -rf => remove any folders ( r means recursivly delete and f means forced )
+
+
