63a8ab40433407ad6262560df5bcca70ad352dc3
vedgohel <vedgohel85@gmail.com>
2026-04-21 15:34:02 +0530

    Git

---
+++ git/script.js
@@ -0,0 +1,51 @@
+// what is git ?  // popular tool
+// free & open source version control system
+
+// tools that help to tracks changes in code
+
+// 1) track history
+// 2) help to collbrate
+
+
+// what is github ?
+// => website where we host repositories online
+
+// readme_md => store info of project
+
+
+// Basic commands
+
+// git clone and status
+
+// clone = > cloning a repository on our local machine => git clone <some link>
+// status = > displays the state of the code => git status
+
+
+// add => adds new or changed files in your working directory to the git staging area. == git add < file-name >
+// commit => it is the record of change == git commit -m "some message"
+// push => upload local repo    content to remote repo === git push origin main
+
+
+
+// init => used to create a new git repo === git init
+
+// git remote add origin < link> 
+// get remote -v
+// git branch
+// git branch -M Main
+// git push origin Main
+
+
+
+// commands 
+
+// git diff main 
+
+// git merge main
+
+
+// pull requested 
+// => it lets you tell others about changes you have pushed to a branch in  a repository  on github
+// => 1 commit
+
+// Forking = making a copy to work separately
