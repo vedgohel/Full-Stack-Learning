a5b66e87b4d520cc4589934dbd10878a036ce0cc
vedgohel <vedgohel85@gmail.com>
2026-04-28 19:44:36 +0530

    SQL PART 1

---
+++ Backend/Sql/Part-1/index1.js
@@ -0,0 +1,4 @@
+// Primary Key                                      	Foreign Key
+// Unique hota hai                                 	Duplicate allowed
+// NULL nahi hota	                                NULL ho sakta hai
+// Table ko identify karta hai	                  Tables ko connect karta hai
